84dca20a75dcf68a7cad314c73a130cbd5da7a15
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-10-03 20:08:26 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,8 +17,8 @@ class Triangle
 
       if @side_one == 0 || @side_two == 0 || @side_three == 0
         raise TriangleError
-      elsif total != 180
-        raise TriangleError
+      #elsif total != 180
+      #  raise TriangleError
       elsif @side_one == 60 && @side_two == 60 && @side_three = 60
         :equilateral
       elsif @side_one == @side_two || @side_one == @side_three || @side_two == @side_three
